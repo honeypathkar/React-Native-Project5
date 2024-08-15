93384423ff223b15bf5b5a0636681f388d3440fb
honeypathkar <honeypatkar70@gmail.com>
2024-08-15 14:54:56 +0530

    Making a object file of music

---
+++ src/constant.ts
@@ -0,0 +1,45 @@
+import {Track} from 'react-native-track-player';
+
+export const playListData: Track[] = [
+  {
+    id: 1,
+    title: '7 Years',
+    artist: 'Lucas Graham',
+    artwork:
+      'https://s.cafebazaar.ir/images/icons/com.shaiban.audioplayer.mplayer-13b17c69-d1c2-4a4e-94d0-e0b32bfa04ee_512x512.png?x-img=v1/resize,h_256,w_256,lossless_false/optimize',
+    url: require('./assests/one.m4a'),
+  },
+  {
+    id: 2,
+    title: 'Baller',
+    artist: 'Shubh',
+    artwork:
+      'https://s.cafebazaar.ir/images/icons/com.shaiban.audioplayer.mplayer-13b17c69-d1c2-4a4e-94d0-e0b32bfa04ee_512x512.png?x-img=v1/resize,h_256,w_256,lossless_false/optimize',
+    url: require('./assets/two.m4a'),
+  },
+  {
+    id: 3,
+    title: 'Cheques',
+    artist: 'Shubh',
+    album: 'Brahmastra',
+    artwork:
+      'https://s.cafebazaar.ir/images/icons/com.shaiban.audioplayer.mplayer-13b17c69-d1c2-4a4e-94d0-e0b32bfa04ee_512x512.png?x-img=v1/resize,h_256,w_256,lossless_false/optimize',
+    url: require('./assets/three.m4a'),
+  },
+  {
+    id: 4,
+    title: 'Her',
+    artist: 'Shubh',
+    artwork:
+      'https://s.cafebazaar.ir/images/icons/com.shaiban.audioplayer.mplayer-13b17c69-d1c2-4a4e-94d0-e0b32bfa04ee_512x512.png?x-img=v1/resize,h_256,w_256,lossless_false/optimize',
+    url: require('./assets/four.m4a'),
+  },
+  {
+    id: 5,
+    title: 'No Love',
+    artist: 'Shubh',
+    artwork:
+      'https://s.cafebazaar.ir/images/icons/com.shaiban.audioplayer.mplayer-13b17c69-d1c2-4a4e-94d0-e0b32bfa04ee_512x512.png?x-img=v1/resize,h_256,w_256,lossless_false/optimize',
+    url: require('./assets/five.m4a'),
+  },
+];
