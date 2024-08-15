4883da0cd3bfad3b3db5079da9da3db065ce90d9
honeypathkar <honeypatkar70@gmail.com>
2024-08-15 14:42:45 +0530

    Making Spotify clone Music file uploaded and project template created

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View>
+      <Text>Spotify Clone</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
